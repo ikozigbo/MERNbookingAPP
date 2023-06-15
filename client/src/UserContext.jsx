82ee04c52c24e05e554badf5489bf51e0e0e45d1
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-06-15 19:35:27 +0100

    15/06

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -5,17 +5,19 @@ export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [ready, setReady] = useState(false);
   useEffect(() => {
     async function fetchData() {
       if (!user) {
         const { data } = await axios.get("/profile");
         setUser(data);
+        setReady(true);
       }
     }
     fetchData();
   }, []);
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user, setUser, ready }}>
       {children}
     </UserContext.Provider>
   );
---
+++ client/src/components/Places.jsx
@@ -0,0 +1,182 @@
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Perks from "./Perks";
+import axios from "axios";
+
+export default function PlacesPage() {
+  const { action } = useParams();
+  const [title, setTile] = useState("");
+  const [address, setAddress] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [photoLink, setPhotoLink] = useState("");
+  const [perks, setPerks] = useState([]);
+  const [description, setDescription] = useState("");
+  const [extraInfo, setExtraInfo] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [maxGuests, setMaxGuests] = useState(1);
+  function preInput(header, description) {
+    return (
+      <>
+        <h2 className="text-2xl mt-4">{header}</h2>
+        <p className="text-gray-500 text-sm">{description}</p>
+      </>
+    );
+  }
+  async function addPhotoByLink(ev) {
+    ev.preventDefault();
+    const { data: filename } = await axios.post("/upload-by-link", {
+      link: photoLink,
+    });
+    setAddedPhotos((prev) => {
+      return [...prev, filename];
+    });
+    setPhotoLink("");
+  }
+  return (
+    <div>
+      {action !== "new" && (
+        <div className="text-center">
+          <Link
+            className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full shadow-md shadow-gray-300"
+            to={"/account/places/new"}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+            Add new place
+          </Link>
+        </div>
+      )}
+      {action === "new" && (
+        <div>
+          <form>
+            {preInput("Title", "title of your place should goes hear")}
+
+            <input
+              type="text"
+              placeholder="title, for example: my lovely apt"
+              value={title}
+              onChange={(ev) => setTile(ev.target.value)}
+            />
+            {preInput("Address", "Address to your place")}
+
+            <input
+              type="text"
+              placeholder="address"
+              value={address}
+              onChange={(ev) => setAddress(ev.target.value)}
+            />
+            {preInput("Photos", "more = better")}
+
+            <div className="flex gap-2">
+              <input
+                value={photoLink}
+                onChange={(ev) => setPhotoLink(ev.target.value)}
+                type="text"
+                placeholder={"add using a link.....jpg"}
+              />
+              <button
+                onClick={addPhotoByLink}
+                className="bg-gray-200 px-4 rounded-2xl"
+              >
+                Add&nbsp;photos
+              </button>
+            </div>
+            <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+              {addedPhotos.length > 0 &&
+                addedPhotos.map((link) => (
+                  <div>
+                    <img
+                      className="rounded-2xl"
+                      src={"http://localhost:9091/uploads/" + link}
+                      alt=""
+                    />
+                  </div>
+                ))}
+              <button className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-8 h-8"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75"
+                  />
+                </svg>
+                Upload
+              </button>
+            </div>
+            {preInput("Description", "description of the place")}
+
+            <textarea
+              value={description}
+              onChange={(ev) => setDescription(ev.target.value)}
+            />
+            {preInput("Perks", "select all the perks of your place")}
+            <div className="grid mt-2  gap-2 grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
+              <Perks selected={perks} onChange={setPerks} />
+            </div>
+            {preInput("Extra info", "house rules, etc")}
+            <textarea
+              value={extraInfo}
+              onChange={(ev) => setExtraInfo(ev.target.value)}
+            />
+            {preInput(
+              "Check in & out times",
+              "add check in and check out times, remember to have some time for cleaning between guests"
+            )}
+
+            <div className="grid gap-2 sm:grid-cols-3">
+              <div>
+                <h3 className="mt-2 -mb-1">Check in time</h3>
+                <input
+                  type="text"
+                  value={checkIn}
+                  onChange={(ev) => setCheckIn(ev.target.value)}
+                  placeholder="14:00"
+                />
+              </div>
+              <div>
+                <h3 className="mt-2 -mb-1">Check out time</h3>
+                <input
+                  type="text"
+                  value={checkOut}
+                  onChange={(ev) => setCheckOut(ev.target.value)}
+                  placeholder="11:00"
+                />
+              </div>
+              <div>
+                <h3 className="mt-2 -mb-1">Max number of guests</h3>
+                <input
+                  type="number"
+                  value={maxGuests}
+                  onChange={(ev) => setMaxGuests(ev.target.value)}
+                />
+              </div>
+            </div>
+            <div>
+              <button className="primary my-4">save</button>
+            </div>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
